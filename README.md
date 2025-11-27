5ec6e839d953c4a0c50a705133d742f4e9400ab0
Khalil <khalilgha812@gmail.com>
2025-11-28 00:09:20 +0100

    add changes

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-# projet-sgbd
-# projet-sgbd
-# projet-sgbd
-# projet-sgbd
+# Projet SQBD
+
+## MongoDB
+
+![MongoDB Logo](./db_logo.jpg)
+
+### Introduction :
+
+Dans le cadre de ce projet, nous présentons un script Python permettant d’établir une connexion avec une base de données MongoDB afin d’effectuer plusieurs opérations fondamentales : l’insertion, la récupération, la mise à jour et la suppression de données.
+L’ensemble de ces opérations sera évalué sur deux environnements distincts : une base de données locale et une instance MongoDB dans le cloud `MongoDB Atlas`.
+
+### Prérequis
+
+Pour exécuter ce script, il est nécessaire de disposer des éléments suivants :
+
+- Python, version 3 ;
+- MongoDB.
+
+Par la suite, l’installation de la bibliothèque du driver MongoDB peut être effectuée à l’aide de la commande :
+`pip install pymongo`
+
+### Exécution du script
+
+Dans le répertoire du projet, le script peut être lancé à l’aide de la commande suivante :
+`python3 main.py`
+
+Une fois exécuté, le script vous invite d’abord à choisir entre l’utilisation d’une instance MongoDB locale ou hébergée dans le cloud. Il affiche ensuite l’ensemble des opérations qui seront réalisées. Il suffit alors d’appuyer sur la touche Entrée pour poursuivre l’exécution.
--- main.py
+++ main.py
@@ -1,29 +1,49 @@
 from pymongo import MongoClient
-from lib.functions import *
+
+def stop(msg) :
+    input(msg)
 
 # version cloud : mongodb+srv://admin:123@projet-sgbd.nj5db3r.mongodb.net/?appName=Projet-sgbd
-client = MongoClient("mongodb://localhost:27017/")
+# version local : mongodb://localhost:27017/
+
+version = input("Souhaitez-vous utiliser le cloud ou rester en local ?\n")
+if version == "cloud" :
+    client = MongoClient("mongodb+srv://admin:123@projet-sgbd.nj5db3r.mongodb.net/?appName=Projet-sgbd")
+elif version == "local" : 
+    client = MongoClient("mongodb://localhost:27017/")
 
 db = client["bd"] # creer ou selectionner base de données
 users = db["users"] # creer ou selectionner une collection
 
 new_user = {
-    "username": "alice",
-    "email": "alice@example.com",
+    "username": "salah",
+    "email": "salah@example.com",
     "age": 18
 }
-
-result = create_user(users,new_user)
+stop("inserer les données de l'utilisateur")
+result = users.insert_one(new_user)
 print("Utilisateur inséré avec l'identifiant:", result.inserted_id)
 
-
-user = find_user(users,{"username" : "alicee"})
+stop("trouver un utilisateur")
+user = users.find_one({"username" : "salah"})
 if user:
     print("Utilisateur trouvé:", user)
 else:
     print("Utilisateur introuvable")
-result = update_user(users,{"username" : "alice"},{"email" : "alice@gmail.com"})
+stop("mise à jour des données utilisateur")
+result = users.update_many({"username" : "salah"},{"$set":{"email" : "salah@gmail.com"}})
 print("Correspondance:", result.matched_count)
 print("Modifié:", result.modified_count)
-result = delete_user(users,{"age" : 18})
+user = users.find_one({"username" : "salah"})
+if user:
+    print("Utilisateur trouvé:", user)
+else:
+    print("Utilisateur introuvable")
+stop("supprimer l'utilisateur")
+result = users.delete_many({"age" : 18})
 print("Supprimé :",result.deleted_count)
+user = users.find_one({"username" : "salah"})
+if user:
+    print("Utilisateur trouvé:", user)
+else:
+    print("Utilisateur introuvable")
